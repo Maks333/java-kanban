6069d57c5151eb915d6076eef99ac0f2b7f035ed
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-02-27 21:37:32 +0700

    Extend Task class functionality

--- src/tasks/Task.java
+++ src/tasks/Task.java
@@ -1,8 +1,56 @@
 package tasks;
 
+import java.util.Objects;
+
 public class Task {
     private String name;
     private String description;
     private int taskID;
     private TaskStatus status;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getTaskID() {
+        return taskID;
+    }
+
+    public void setTaskID(int taskID) {
+        this.taskID = taskID;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return taskID == task.taskID && Objects.equals(name, task.name)
+                && Objects.equals(description, task.description) && status == task.status;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description, taskID, status);
+    }
 }
