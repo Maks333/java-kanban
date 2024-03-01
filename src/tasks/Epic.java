445581e93f0dc3ee45c6f3eade4b923d6de436cd
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-03-01 16:40:45 +0700

    Implement epics constructors

--- src/tasks/Epic.java
+++ src/tasks/Epic.java
@@ -2,5 +2,13 @@ package tasks;
 import java.util.ArrayList;
 
 public class Epic extends Task{
-    ArrayList<SubTask> subTasks;
+    ArrayList<SubTask> subTasks = new ArrayList<>();
+
+    public Epic(String name, String description, int taskID) {
+        super(name, description, taskID, TaskStatus.NEW);
+    }
+
+    public Epic(String name, String description) {
+        this(name, description, 0);
+    }
 }
