22424e9aaf9bf55815865349f999bf3ffe41ee43
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-03-01 11:01:16 +0700

    Implement SubTask constructors

--- src/tasks/SubTask.java
+++ src/tasks/SubTask.java
@@ -2,4 +2,13 @@ package tasks;
 
 public class SubTask extends Task{
     int epicID;
+
+    public SubTask(String name, String description, int taskID, TaskStatus status, int epicID) {
+        super(name, description, taskID, status);
+        this.epicID = epicID;
+    }
+
+    public SubTask(String name, String description, TaskStatus status, int epicID) {
+        this(name, description, 0, status, epicID);
+    }
 }
