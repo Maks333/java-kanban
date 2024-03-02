a189df5f99c43d381cad4b16610934e29b07a36e
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-03-03 00:02:39 +0700

    Demonstrate work of taskManager program

--- src/Main.java
+++ src/Main.java
@@ -26,5 +26,34 @@ public class Main {
         System.out.println(taskManager.getAllTasks());
         System.out.println(taskManager.getAllSubtasks());
         System.out.println(taskManager.getAllEpics());
+
+        System.out.println("\n\n\n-------------------UPDATING STATUSES-----------------------------");
+        task1 = new Task(task1.getName(), task1.getDescription(), task1.getTaskID(), TaskStatus.DONE);
+        task2 = new Task(task2.getName(), task2.getDescription(), task2.getTaskID(), TaskStatus.IN_PROGRESS);
+        taskManager.updateTask(task1);
+        taskManager.updateTask(task2);
+
+        subTask1 = new SubTask(subTask1.getName(), subTask1.getDescription(), subTask1.getTaskID(),
+                TaskStatus.DONE, subTask1.getEpicID());
+        taskManager.updateSubTask(subTask1);
+        subTask2 = new SubTask(subTask2.getName(), subTask2.getDescription(), subTask2.getTaskID(),
+                TaskStatus.DONE, subTask2.getEpicID());
+        taskManager.updateSubTask(subTask2);
+
+        subTask3 = new SubTask(subTask3.getName(), subTask3.getDescription(), subTask3.getTaskID(),
+                TaskStatus.IN_PROGRESS, subTask3.getEpicID());
+        taskManager.updateSubTask(subTask3);
+
+        System.out.println(taskManager.getAllTasks());
+        System.out.println(taskManager.getAllSubtasks());
+        System.out.println(taskManager.getAllEpics());
+
+        System.out.println("\n\n\n------------------REMOVING ONE TASK AND ONE EPIC--------------------------");
+        taskManager.deleteTaskById(task1.getTaskID());
+        taskManager.deleteEpicByID(epic1.getTaskID());
+
+        System.out.println(taskManager.getAllTasks());
+        System.out.println(taskManager.getAllSubtasks());
+        System.out.println(taskManager.getAllEpics());
     }
 }
