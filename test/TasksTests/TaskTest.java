fae553b43ef9db86b3851eb41029e9c7799ac7ac
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-03-16 13:02:21 +0700

    Create Tests for tasks equality

--- test/TasksTests/TaskTest.java
+++ test/TasksTests/TaskTest.java
@@ -1,4 +1,26 @@
 package TasksTests;
 
+import org.junit.jupiter.api.Test;
+import tasks.Task;
+import tasks.TaskStatus;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TaskTest {
+
+    @Test
+    public void twoTaskWithSameIdShouldBeEqual() {
+        Task task1 = new Task("Task1Name", "Task1Description", 1, TaskStatus.NEW);
+        Task task2 = new Task("Task2Name", "Task2Description", 1, TaskStatus.NEW);
+
+        assertEquals(task1, task2, "Two tasks with same id should be equal");
+    }
+
+    @Test
+    public void twoTasksWithSameIdShouldNotBeEquals() {
+        Task task1 = new Task("Task1Name", "Task1Description", 1, TaskStatus.NEW);
+        Task task2 = new Task("Task2Name", "Task2Description", 2, TaskStatus.NEW);
+
+        assertNotEquals(task1, task2, "Two tasks with same id should not be equal");
+    }
 }
