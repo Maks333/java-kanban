0f13067bb5db17cf813a5ee1cfb4cecedec04f72
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-03-16 13:48:12 +0700

    Create Tests for subtasks equality

--- test/EpicsTests/EpicTest.java
+++ test/EpicsTests/EpicTest.java
@@ -2,8 +2,6 @@ package EpicsTests;
 
 import org.junit.jupiter.api.Test;
 import tasks.Epic;
-import tasks.Task;
-import tasks.TaskStatus;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
@@ -12,8 +10,8 @@ public class EpicTest {
 
     @Test
     public void twoEpicsWithSameIdShouldBeEqual() {
-        Epic epic1 = new Epic("Task1Name", "Epic1Description", 1);
-        Epic epic2 = new Epic("Task2Name", "Epic2Description", 1);
+        Epic epic1 = new Epic("Epic1Name", "Epic1Description", 1);
+        Epic epic2 = new Epic("Epic2Name", "Epic2Description", 1);
 
         assertEquals(epic1, epic2, "Two epics with same id should be equal");
     }
