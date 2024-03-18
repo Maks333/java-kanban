adee3caf5d36f26147c0677239beacdd0e70e941
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-03-18 20:24:17 +0700

    Create test for two Task with same id equality

---
+++ test/TasksManagersTests/ManagerWithTasksTest.java
@@ -0,0 +1,27 @@
+package TasksManagersTests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import tasks.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ManagerWithTasksTest {
+    private TaskManager manager;
+
+    @BeforeEach
+    void beforeEach() {
+        manager = Managers.getDefault();
+    }
+
+    @Test
+    void twoTasksWithSameIdShouldBeEqual() {
+        Task Task1 = new Task("Task1Name", "Task1Description", TaskStatus.NEW);
+        int Task1ID = manager.createTask(Task1);
+
+        Task Task2 = new Task("Task2Name", "Task2Description", Task1ID, TaskStatus.NEW);
+
+        manager.updateTask(Task2);
+        assertEquals(manager.getTaskById(Task1ID), Task2);
+    }
+}
