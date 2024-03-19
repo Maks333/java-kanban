6d552c82bbe5f90f125b1d057d31918e7c3e48b2
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-03-19 18:47:42 +0700

    Remove abundance casting in add method of InMemoryHistoryManager class

--- src/Managers/InMemoryHistoryManager.java
+++ src/Managers/InMemoryHistoryManager.java
@@ -15,20 +15,12 @@ public class InMemoryHistoryManager implements HistoryManager {
     @Override
     public void add(Task task) {
         if (task == null) return;
-        Task taskToAdd;
-        if (task.getClass() == Task.class) {
-            taskToAdd = new Task(task);
-        } else if (task.getClass() == SubTask.class) {
-            taskToAdd = new SubTask((SubTask)task);
-        } else {
-            taskToAdd = new Epic((Epic)task);
-        }
 
         final int MAX_HISTORY_SIZE = 10;
         if (history.size() >= MAX_HISTORY_SIZE) {
             history.removeFirst();
         }
-        history.add(taskToAdd);
+        history.add(task);
     }
 
     @Override
