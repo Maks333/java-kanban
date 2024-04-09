8085bdb1ff60b7ad334dd938169a4dd2a848900e
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-04-09 13:08:53 +0700

    Implement linkLast method

--- src/Managers/InMemoryHistoryManager.java
+++ src/Managers/InMemoryHistoryManager.java
@@ -9,13 +9,13 @@ import java.util.List;
 public class InMemoryHistoryManager implements HistoryManager {
     private final ArrayList<Task> history;
 
-    private Node head;
-    private Node tail;
+    private Node head = null;
+    private Node tail = null;
 
     private final HashMap<Integer, Node> nodeById = new HashMap<>();
 
     class Node {
-        Node next;
+        Node next = null;
         Node previous;
         Task task;
         Node(Node previous, Task task) {
@@ -29,7 +29,15 @@ public class InMemoryHistoryManager implements HistoryManager {
     }
 
     private void linkLast(Task task) {
-
+        if (tail == null && head == null) {
+            Node node = new Node(null, task);
+            tail = node;
+            head = node;
+        } else {
+            Node node = new Node(tail, task);
+            tail.next = node;
+            tail = node;
+        }
     }
 
     private List<Task> getTasks() {
