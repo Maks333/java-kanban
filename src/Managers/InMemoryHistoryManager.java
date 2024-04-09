0d7623cfa854a70117d8df43ae6b55931fe20642
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-04-09 12:41:50 +0700

    Create LinkedList Node class

--- src/Managers/InMemoryHistoryManager.java
+++ src/Managers/InMemoryHistoryManager.java
@@ -8,6 +8,16 @@ import java.util.List;
 public class InMemoryHistoryManager implements HistoryManager {
     private final ArrayList<Task> history;
 
+    class Node {
+        Node next;
+        Node previous;
+        Task task;
+        Node(Node previous, Task task) {
+            this.previous = previous;
+            this.task = task;
+        }
+    }
+
     public InMemoryHistoryManager() {
         history = new ArrayList<>();
     }
