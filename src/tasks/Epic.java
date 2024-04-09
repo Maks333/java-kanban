266de64f7339ee1e3846c7842ad8fd1cd7d66ca9
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-04-09 18:42:32 +0700

    Add white space before figure bracket in Epic class

--- src/tasks/Epic.java
+++ src/tasks/Epic.java
@@ -2,7 +2,7 @@ package tasks;
 
 import java.util.ArrayList;
 
-public class Epic extends Task{
+public class Epic extends Task {
     private final ArrayList<Integer> subTasks;
 
     public Epic(String name, String description, int taskID) {
