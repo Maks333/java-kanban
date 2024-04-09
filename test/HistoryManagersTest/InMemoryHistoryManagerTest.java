a0d7076e1cedc5394d29b618813f94ab706ffd83
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-04-09 16:48:45 +0700

    Prepare test bodies in InMemoryHistoryManagerTest class

--- test/HistoryManagersTest/InMemoryHistoryManagerTest.java
+++ test/HistoryManagersTest/InMemoryHistoryManagerTest.java
@@ -1,10 +1,12 @@
 package HistoryManagersTest;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import Managers.HistoryManager;
 import Managers.Managers;
 import Tasks.Task;
 import Tasks.TaskStatus;
+import Managers.TaskManager;
 
 import java.util.List;
 
@@ -13,6 +15,12 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class InMemoryHistoryManagerTest {
 
+    private TaskManager manager;
+
+    @BeforeEach
+    public void beforeEach() {
+        manager = Managers.getDefault();
+    }
 
     @Test
     public void HistoryShouldRemoveDuplicates() {
@@ -34,4 +42,36 @@ public class InMemoryHistoryManagerTest {
         assertEquals(1, historyAfterAddition.size());
         assertEquals(historyAfterAddition.getFirst(), task);
     }
+
+    @Test
+    public void RemovingFromManagerAlsoRemoveTaskFromHistory() {}
+    @Test
+    public void HistoryCorrectlyAddOneTask() {}
+
+    @Test
+    public void HistoryCorrectlyAddTwoTasks() {}
+
+    @Test
+    public void HistoryCorrectlyRemoveOneTask() {}
+
+    @Test
+    public void HistoryCorrectlyRemoveHeadTask() {}
+
+    @Test
+    public void HistoryCorrectlyRemoveTailTask() {}
+
+    @Test
+    public void HistoryCorrectlyRemoveTaskFromTheMiddle() {}
+
+    @Test
+    public void ShouldRemoveTaskFromManagerAndHistory() {}
+
+    @Test
+    public void ShouldRemoveSubTaskFromManagerAndHistory() {}
+
+    @Test
+    public void ShouldRemoveEpicFromManagerAndHistory() {}
+
+    @Test
+    public void ShouldRemoveEpicWithSubTaskFromManagerAndHistory() {}
 }
