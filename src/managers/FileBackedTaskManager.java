e01a01a18494b5f8f94b2017e05c9305e662e43e
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-04-20 22:40:03 +0700

    Add auto-saving method in all methods that modify taskManager

--- src/managers/FileBackedTaskManager.java
+++ src/managers/FileBackedTaskManager.java
@@ -1,5 +1,49 @@
 package managers;
 
-public class FileBackedTaskManager extends InMemoryTaskManager {
+import tasks.Epic;
+import tasks.SubTask;
+import tasks.Task;
 
+public class FileBackedTaskManager extends InMemoryTaskManager {
+    @Override
+    public void updateEpic(Epic newEpic) {
+        super.updateEpic(newEpic);
+        save();
+    }
+
+    @Override
+    public int createEpic(Epic newEpic) {
+        int id = super.createEpic(newEpic);
+        save();
+        return id;
+    }
+
+    @Override
+    public int createSubTask(SubTask newSubTask) {
+        int id = super.createSubTask(newSubTask);
+        save();
+        return id;
+    }
+
+    @Override
+    public void updateSubTask(SubTask newSubTask) {
+        super.updateSubTask(newSubTask);
+        save();
+    }
+
+    @Override
+    public void updateTask(Task newTask) {
+        super.updateTask(newTask);
+        save();
+    }
+
+    @Override
+    public int createTask(Task newTask) {
+        int id = super.createTask(newTask);
+        save();
+        return id;
+    }
+
+    private void save() {
+    }
 }
