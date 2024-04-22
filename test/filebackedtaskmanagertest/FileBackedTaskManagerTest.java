c4335866be6a0929af90caacc7a81e1429e51ae4
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-04-22 09:50:13 +0700

    Create shouldProperlyLoadFromEmptyFile test

--- test/filebackedtaskmanagertest/FileBackedTaskManagerTest.java
+++ test/filebackedtaskmanagertest/FileBackedTaskManagerTest.java
@@ -3,6 +3,7 @@ package filebackedtaskmanagertest;
 import managers.FileBackedTaskManager;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import tasks.*;
 
 import java.io.File;
@@ -18,19 +19,41 @@ public class FileBackedTaskManagerTest {
     SubTask subTask;
     Epic epic;
 
-    @BeforeAll
-    static void beforeAll() {
+    @BeforeEach
+    void beforeEach() {
         try {
             file = Files.createTempFile("example", ".txt").toFile();
         } catch (IOException e) {
             System.out.println("Error during file creation");
         }
-    }
-
-    @BeforeEach
-    void beforeEach() {
         manager = new FileBackedTaskManager(file);
         task = new Task("task", "task desc", TaskStatus.NEW);
         epic = new Epic("epic", "epic desc");
+    }
+
+    @Test
+    void shouldProperlyLoadFromEmptyFile() {
+        FileBackedTaskManager newManager;
+        try {
+            String content = Files.readString(file.toPath());
+            assertEquals(content, "", "Should be empty");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        newManager = FileBackedTaskManager.loadFromFile(file);
+
+        assertNotNull(newManager, "Manager should be initialized");
+        assertEquals(0, newManager.getAllTasks().size(), "Should be without tasks");
+        assertEquals(0, newManager.getAllSubtasks().size(), "Should be without subtasks");
+        assertEquals(0, newManager.getAllEpics().size(), "Should be without epics");
+    }
+
+    @Test
+    void shouldProperlySaveSeveralTasks() {
+    }
+
+    @Test
+    void shouldProperlyLoadSeveralTasks() {
     }
 }
