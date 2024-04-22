89e32520e463f83adb889758d1cf4f09e8d4ea88
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-04-22 10:06:21 +0700

    Create shouldProperlySaveAndLoadSeveralTasks test

--- test/filebackedtaskmanagertest/FileBackedTaskManagerTest.java
+++ test/filebackedtaskmanagertest/FileBackedTaskManagerTest.java
@@ -1,7 +1,6 @@
 package filebackedtaskmanagertest;
 
 import managers.FileBackedTaskManager;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import tasks.*;
@@ -9,6 +8,7 @@ import tasks.*;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -50,10 +50,31 @@ public class FileBackedTaskManagerTest {
     }
 
     @Test
-    void shouldProperlySaveSeveralTasks() {
-    }
+    void shouldProperlySaveAndLoadSeveralTasks() {
+        manager.createTask(task);
+        int epicID = manager.createEpic(epic);
+        subTask = new SubTask("subtask", "subtask desc", TaskStatus.NEW, epicID);
+        manager.createSubTask(subTask);
 
-    @Test
-    void shouldProperlyLoadSeveralTasks() {
+        try {
+            String content = Files.readString(file.toPath());
+            assertNotEquals("", content, "Should not be empty");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Task savedTask = manager.getAllTasks().getFirst();
+        SubTask savedSubTask = manager.getAllSubtasks().getFirst();
+        Epic savedEpic = manager.getAllEpics().getFirst();
+
+        manager = FileBackedTaskManager.loadFromFile(file);
+        assertNotNull(manager, "Should be initialized");
+        assertEquals(1, manager.getAllTasks().size(), "Should be 1 Task");
+        assertEquals(savedTask, manager.getAllTasks().getFirst(), "Should be equal to previous version");
+
+        assertEquals(1, manager.getAllEpics().size(), "Should be 1 Epic");
+        assertEquals(savedEpic, manager.getAllEpics().getFirst(), "Should be equal to previous version");
+
+        assertEquals(1, manager.getAllSubtasks().size(), "Should be 1 SubTask");
+        assertEquals(savedSubTask, manager.getAllSubtasks().getFirst(), "Should be equal to previous version");
     }
 }
