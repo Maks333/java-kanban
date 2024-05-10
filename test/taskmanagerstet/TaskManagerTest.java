7f58293a502e2db93d880d6f3886ef20baf22026
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-05-10 19:34:55 +0700

    Add testcases for SubTask class in TaskManagerTest

--- test/taskmanagerstet/TaskManagerTest.java
+++ test/taskmanagerstet/TaskManagerTest.java
@@ -42,6 +42,7 @@ public abstract class TaskManagerTest<T extends TaskManager> {
         subTask4 = new SubTask("subTask4", "subTask4Desc", 9, TaskStatus.NEW, 8);
     }
 
+    //Start of Task testing section
     @Test
     void twoTasksWithSameIdShouldBeEqual() {
         int Task1ID = manager.createTask(task1);
@@ -168,4 +169,175 @@ public abstract class TaskManagerTest<T extends TaskManager> {
         assertNotNull(tasks, "There should be 2 Tasks");
         assertEquals(2, tasks.size(), "Incorrect number of tasks");
     }
+    //End of Task testing section
+
+    //Start of SubTask testing section
+    @Test
+    void cannotMakeSubTaskEpicOfItself() {
+        manager.createEpic(epic1);
+        int subTaskId = manager.createSubTask(subTask1);
+
+        SubTask newSubTask = new SubTask("NewSubTaskName", "NewSubTaskDescription", subTaskId,
+                TaskStatus.NEW, 4);
+
+        manager.updateSubTask(newSubTask);
+
+        subTask1 = manager.getSubTaskById(subTaskId);
+
+        assertNotNull(subTask1, "SubTask should be in the TaskManager");
+        assertNotEquals(subTask1.getTaskId(), subTask1.getEpicId(), "SubTask id and epicId cannot be equal");
+        assertEquals(4, subTask1.getEpicId(), "Epic id should be the same");
+    }
+
+    @Test
+    void twoSubTasksWithSameIdShouldBeEqual() {
+        int epicId = manager.createEpic(epic1);
+        int subTask1ID = manager.createSubTask(subTask1);
+
+        SubTask subTask2 = new SubTask("SubTask2Name", "SubTask2Description", subTask1ID, TaskStatus.NEW,
+                epicId);
+
+        manager.updateSubTask(subTask2);
+        assertEquals(manager.getSubTaskById(subTask1ID), subTask2, "SubTask should have the same id");
+    }
+
+    @Test
+    void twoSubTasksWithDifferentIdShouldNotBeEqual() {
+        manager.createEpic(epic1);
+        int subTask1ID = manager.createSubTask(subTask1);
+
+        SubTask subTask2 = new SubTask("SubTask2Name", "SubTask2Description", 15, TaskStatus.NEW,
+                epic1.getTaskId());
+
+        manager.updateSubTask(subTask2);
+        assertNotEquals(manager.getSubTaskById(subTask1ID), subTask2, "Should not have the same id");
+    }
+
+    @Test
+    void addNewSubTask() {
+        manager.createEpic(epic1);
+        int subTask1Id = manager.createSubTask(subTask1);
+
+        SubTask savedSubTask = manager.getSubTaskById(subTask1Id);
+        assertNotNull(savedSubTask, "SubTask should be in the manager");
+        assertEquals(subTask1, savedSubTask, "SubTasks should be equal");
+
+        List<SubTask> subTasks = manager.getAllSubtasks();
+
+        assertNotNull(subTasks, "There should be 1 subtask");
+        assertEquals(1, subTasks.size(), "Incorrect number of subtasks");
+        assertEquals(subTask1, subTasks.getFirst(), "SubTasks aren't equal");
+    }
+
+    @Test
+    void SubTasksWithGeneratedIdAndAssignedIdShouldNotConflict() {
+        int epicID = manager.createEpic(epic1);
+        SubTask generatedSubTask = new SubTask("GeneratedSubTaskName", "GeneratedSubTaskDesc",
+                TaskStatus.NEW, epicID);
+        int generatedSubTaskId = manager.createSubTask(generatedSubTask);
+
+        int assignedSubTaskId = manager.createSubTask(subTask1);
+
+        SubTask savedGeneratedSubtask = manager.getSubTaskById(generatedSubTaskId);
+        SubTask savedAssignedSubTask = manager.getSubTaskById(assignedSubTaskId);
+
+        assertNotNull(savedGeneratedSubtask, "SubTask with generated id should be in the system");
+        assertNotNull(savedAssignedSubTask, "SubTask with assigned id should be in the system");
+        assertNotEquals(savedGeneratedSubtask.getTaskId(), savedAssignedSubTask.getTaskId(),
+                "Id should not conflict");
+    }
+
+    @Test
+    void SubTaskShouldBeTheSameAfterAdditionInTaskManager() {
+        manager.createEpic(epic1);
+        int subTaskId = manager.createSubTask(subTask1);
+
+        SubTask subTaskAfterAddition = manager.getSubTaskById(subTaskId);
+
+        assertNotNull(subTaskAfterAddition, "SubTask should be in the manager");
+        assertEquals(subTask1, subTaskAfterAddition, "SubTask should remain the same after" +
+                " addition");
+        assertEquals(subTask1.getEpicId(), subTaskAfterAddition.getEpicId(), "Should have the same id");
+        assertEquals(subTask1.getName(), subTaskAfterAddition.getName(), "Should have the same name");
+        assertEquals(subTask1.getDescription(), subTaskAfterAddition.getDescription(), "Should have " +
+                "the same description");
+        assertEquals(subTask1.getStatus(), subTaskAfterAddition.getStatus(), "Should have the same status");
+    }
+
+    @Test
+    void shouldRemoveSubTaskWithExistingId() {
+        manager.createEpic(epic1);
+        int subTask1Id = manager.createSubTask(subTask1);
+
+        List<SubTask> subTasks = manager.getAllSubtasks();
+        assertNotNull(subTasks, "Should be one SubTask");
+        assertEquals(1, subTasks.size(), "Should be one SubTask");
+
+        manager.deleteSubTaskById(subTask1Id);
+
+        SubTask notExistingSubTask = manager.getSubTaskById(subTask1Id);
+        assertNull(notExistingSubTask, "Should be removed from the system");
+
+        List<SubTask> subTasksAfterDeletion = manager.getAllSubtasks();
+        assertTrue(subTasksAfterDeletion.isEmpty(), "SubTask list should be empty");
+    }
+
+    @Test
+    void shouldAddSubTaskWithIdIfIdIsNotInTheSystem() {
+        int epicId = manager.createEpic(epic1);
+        SubTask subTask = new SubTask("SubTask1Name", "SubTask1Description", 15,
+                TaskStatus.NEW, epicId);
+        int subTaskId = manager.createSubTask(subTask);
+        assertEquals(subTaskId, subTask.getTaskId(), "There is not task that occupies that id");
+
+        Task savedTask = manager.getSubTaskById(subTaskId);
+        assertNotNull(savedTask, "Task isn't found");
+        List<SubTask> tasks = manager.getAllSubtasks();
+
+        assertNotNull(tasks, "There should be 1 SubTask");
+        assertEquals(1, tasks.size(), "Incorrect number of subTasks");
+        assertEquals(savedTask, tasks.getFirst(), "SubTasks aren't equal");
+    }
+
+    @Test
+    void shouldAddSubTaskWithAssignedIdIfIdIsOccupied() {
+        int epicId = manager.createEpic(epic1);
+        SubTask subTask = new SubTask("SubTask1Name", "SubTask1Description", 15,
+                TaskStatus.NEW, epicId);
+        int subTaskId = manager.createSubTask(subTask);
+        assertEquals(subTaskId, subTask.getTaskId(), "There is not task that occupies that id");
+
+        SubTask subTask1 = new SubTask("SubTask2Name", "SubTask2Description", 15,
+                TaskStatus.NEW, epicId);
+        int subTask1Id = manager.createSubTask(subTask1);
+        assertEquals(16, subTask1Id, "Should be equal to 16");
+
+        Task savedTask = manager.getSubTaskById(subTaskId);
+        assertNotNull(savedTask, "Task isn't found");
+        List<SubTask> tasks = manager.getAllSubtasks();
+
+        assertNotNull(tasks, "There should be 2 SubTask");
+        assertEquals(2, tasks.size(), "Incorrect number of subTasks");
+    }
+
+    @Test
+    void shouldContinueAssigningIdFromCurrentMaxValueWithSubTasks() {
+        int epicId = manager.createEpic(epic1);
+        SubTask subTask = new SubTask("SubTask1Name", "SubTask1Description", 15,
+                TaskStatus.NEW, epicId);
+        int subTaskId = manager.createSubTask(subTask);
+        assertEquals(subTaskId, subTask.getTaskId(), "There is not task that occupies that id");
+
+        SubTask subTask1 = new SubTask("SubTask2Name", "SubTask2Description", TaskStatus.NEW, epicId);
+        int subTask1Id = manager.createSubTask(subTask1);
+        assertEquals(16, subTask1Id, "Should be equal to 16");
+
+        Task savedTask = manager.getSubTaskById(subTaskId);
+        assertNotNull(savedTask, "Task isn't found");
+        List<SubTask> tasks = manager.getAllSubtasks();
+
+        assertNotNull(tasks, "There should be 2 SubTask");
+        assertEquals(2, tasks.size(), "Incorrect number of subTasks");
+    }
+    //End of SubTask testing section
 }
