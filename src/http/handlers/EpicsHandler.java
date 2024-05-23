473fa0a794784478160ae29e46348bb33f9e1375
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-05-23 15:17:21 +0700

    Create base version of EpicsHandler

---
+++ src/http/handlers/EpicsHandler.java
@@ -0,0 +1,12 @@
+package http.handlers;
+
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+
+public class EpicsHandler extends BaseHttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+
+    }
+}
