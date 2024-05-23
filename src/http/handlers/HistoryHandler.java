041561b20a65f02a45fb648f5653ee13973a92f7
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-05-23 15:17:53 +0700

    Create base version of HistoryHandler

---
+++ src/http/handlers/HistoryHandler.java
@@ -0,0 +1,12 @@
+package http.handlers;
+
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+
+public class HistoryHandler extends BaseHttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+
+    }
+}
