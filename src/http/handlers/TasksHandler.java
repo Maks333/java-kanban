2ae3211d448b6163db94a1c4ac607bfb53bd19e6
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-05-23 15:15:25 +0700

    Create base version of TasksHandler

---
+++ src/http/handlers/TasksHandler.java
@@ -0,0 +1,12 @@
+package http.handlers;
+
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+
+public class TasksHandler extends BaseHttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+
+    }
+}
