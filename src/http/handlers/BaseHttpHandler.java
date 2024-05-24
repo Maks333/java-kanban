ee0779f0de4d1b512c5359a7109b45752e643250
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-05-24 11:32:01 +0700

    Add sendText method in BaseHttpHandler class

--- src/http/handlers/BaseHttpHandler.java
+++ src/http/handlers/BaseHttpHandler.java
@@ -1,12 +1,27 @@
 package http.handlers;
 
+import com.google.gson.Gson;
+import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import managers.TaskManager;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 abstract public class BaseHttpHandler implements HttpHandler {
     TaskManager manager;
+    Gson gson;
 
-    public BaseHttpHandler(TaskManager manager) {
+    public BaseHttpHandler(TaskManager manager, Gson gson) {
         this.manager = manager;
+        this.gson = gson;
+    }
+
+    protected void sendText(HttpExchange h, String text) throws IOException {
+        byte[] resp = text.getBytes(StandardCharsets.UTF_8);
+        h.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
+        h.sendResponseHeaders(200, resp.length);
+        h.getResponseBody().write(resp);
+        h.close();
     }
 }
