ee1009afe3e5bd344263ffcb8b9ba7193c921069
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-05-24 11:54:29 +0700

    Implement GET /tasks{id} endpoint

--- src/http/handlers/TasksHandler.java
+++ src/http/handlers/TasksHandler.java
@@ -3,6 +3,7 @@ package http.handlers;
 import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
 import managers.TaskManager;
+import tasks.Task;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -16,25 +17,33 @@ public class TasksHandler extends BaseHttpHandler {
     public void handle(HttpExchange exchange) throws IOException {
         System.out.println("Start handle /task handler");
         //Get request method
-        String method = exchange.getRequestMethod();
-        String[] uri = exchange.getRequestURI().getPath().split("/");
-        System.out.println(Arrays.toString(uri));
+        try {
+            String method = exchange.getRequestMethod();
+            String[] uri = exchange.getRequestURI().getPath().split("/");
+            System.out.println(Arrays.toString(uri));
 
-        switch (method) {
-            case "GET":
-                if (uri.length == 2) {
-                    String allTasksJson = gson.toJson(manager.getAllTasks());
-                    sendText(exchange, allTasksJson);
-                } else {
-
-                }
-                break;
-            case "POST":
-                break;
-            case "DELETE":
-                break;
-            default:
-
+            switch (method) {
+                case "GET":
+                    if (uri.length == 2) {
+                        String allTasksJson = gson.toJson(manager.getAllTasks());
+                        sendText(exchange, allTasksJson);
+                    } else {
+                        int taskId = Integer.parseInt(uri[2]);
+                        Task task = manager.getTaskById(taskId);
+                        String taskJson = gson.toJson(task);
+                        sendText(exchange, taskJson);
+                    }
+                    break;
+                case "POST":
+                    break;
+                case "DELETE":
+                    break;
+                default:
+            }
+        } catch (NumberFormatException ex) {
+            sendBadRequest(exchange, "Bad request");
+        } catch (Exception ex) {
+            System.out.println(ex.getClass());
         }
     }
 }
