1bd5daa20a55a79e61ae66ddf6d59fa51ea0adcd
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-05-25 21:29:57 +0700

    Add GET /prioritized endpoint

--- src/http/handlers/PrioritizedTaskHandler.java
+++ src/http/handlers/PrioritizedTaskHandler.java
@@ -2,9 +2,11 @@ package http.handlers;
 
 import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
+import exceptions.UnknownHTTPMethodException;
 import managers.TaskManager;
 
 import java.io.IOException;
+import java.nio.file.InvalidPathException;
 
 public class PrioritizedTaskHandler extends BaseHttpHandler {
     public PrioritizedTaskHandler(TaskManager manager, Gson gson) {
@@ -13,6 +15,22 @@ public class PrioritizedTaskHandler extends BaseHttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
+        try {
+            String method = exchange.getRequestMethod();
+            String[] uri = exchange.getRequestURI().getPath().split("/");
 
+            if (method.equals("GET")) {
+                if (uri.length == 2) {
+                    String prioritizedTaskJson = gson.toJson(manager.getPrioritizedTasks());
+                    sendText(exchange, prioritizedTaskJson, 200);
+                } else {
+                    throw new InvalidPathException(exchange.getRequestURI().getPath(), "There is no such endpoint: ");
+                }
+            } else {
+                throw new UnknownHTTPMethodException("Unknown HTTP method: " + method);
+            }
+        } catch (UnknownHTTPMethodException | IllegalArgumentException ex) {
+            sendBadRequest(exchange, ex.getMessage());
+        }
     }
 }
